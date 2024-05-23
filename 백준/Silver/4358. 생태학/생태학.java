a7fd8fe7ca9158753e6f8f14e068b0b6b090b419
chooorock <105736179+chorockchung@users.noreply.github.com>
2024-05-23 16:28:00 +0900

    [Silver II] Title: 생태학, Time: 1016 ms, Memory: 101916 KB -BaekjoonHub

---
+++ 백준/Silver/4358. 생태학/생태학.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		Map<String, Integer> map = new HashMap<>();
+		String tree;
+		int species = 0;
+		while((tree = br.readLine()) != null) {
+			if(map.containsKey(tree)) {
+				int value = map.get(tree);
+				map.replace(tree, value+1);
+			}
+			else
+				map.put(tree, 1);
+			species++;
+		}
+		
+		List<String> list = new ArrayList<>();
+		for(String s : map.keySet()) {
+			list.add(s);
+		}
+		Collections.sort(list);
+		
+		for(String s: list) {
+			System.out.print(s + " ");
+            System.out.printf("%.4f\n", (map.get(s) * 100.0 / species));
+		}
+	}
+}
