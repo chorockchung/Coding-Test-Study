4b3369ed4def51de8b7f26fc476271127005c4e2
chooorock <105736179+chorockchung@users.noreply.github.com>
2024-05-27 17:06:50 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		String[] pattern = br.readLine().split("\\*");
+		int patternLen = pattern[0].length() + pattern[1].length();
+		
+		for(int i = 0; i < n; i++) {
+			String str = br.readLine();
+			if(str.length() < patternLen || str.indexOf(pattern[0]) == str.lastIndexOf(pattern[1]))
+				sb.append("NE").append("\n");
+			else if(str.indexOf(pattern[0]) == 0 && str.lastIndexOf(pattern[1]) == str.length()-pattern[1].length()) 
+				sb.append("DA").append("\n");
+			else
+				sb.append("NE").append("\n");
+		}
+		
+		System.out.print(sb);
+	}
+}
