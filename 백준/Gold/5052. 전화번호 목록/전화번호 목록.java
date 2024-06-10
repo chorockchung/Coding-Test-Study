6dc69ca7003eda04ee641af1226f1a06de187dce
chooorock <105736179+chorockchung@users.noreply.github.com>
2024-06-10 16:27:03 +0900

    [Gold IV] Title: 전화번호 목록, Time: 304 ms, Memory: 101380 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -1,10 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 class Node {
 	Node[] child;
@@ -46,11 +42,11 @@ class Trie {
 				current = current.child[chIdx];
 		}
 		return true;
-	}
-	
+	}	
 }
-public class Main {	
 
+public class Main {	
+    
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -72,8 +68,8 @@ public class Main {
 				sb.append("YES");
 			sb.append('\n');
 		}
-
+        
 		System.out.println(sb);
-
 	}
+    
 }
