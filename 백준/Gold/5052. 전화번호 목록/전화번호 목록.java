4e28163a56a0e150cdf277cc3c28a75d4d355f8a
chooorock <105736179+chorockchung@users.noreply.github.com>
2024-06-10 16:17:21 +0900

    [Gold IV] Title: 전화번호 목록, Time: 316 ms, Memory: 101536 KB -BaekjoonHub

---
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,79 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+class Node {
+	Node[] child;
+	boolean eow;
+	
+	Node() {
+		child = new Node[10];
+		eow = false;
+	}
+}
+
+class Trie {
+	Node root;
+	
+	Trie() {
+		root = new Node();
+	}
+	
+	boolean insert(String str) {		
+		Node current = this.root;
+		int chIdx = ' ';
+		boolean flag = false;
+		for(int i = 0; i < str.length(); i++) {
+			chIdx = str.charAt(i) - '0';
+			
+			if(current.child[chIdx] == null) {
+				current.child[chIdx] = new Node();
+				flag = true;
+			}		
+			
+			if(current.child[chIdx].eow) 
+				return false;
+			if(i == str.length() - 1) {
+				current.child[chIdx].eow = true;
+				if(!flag)
+					return false;
+			}
+			else
+				current = current.child[chIdx];
+		}
+		return true;
+	}
+	
+}
+public class Main {	
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+						
+		int caseNum = Integer.parseInt(br.readLine());
+		for(int i = 0; i < caseNum; i++) {
+			Trie trie = new Trie();
+			int n = Integer.parseInt(br.readLine());
+			boolean rslt = true;
+			for(int j = 0; j < n; j++) {
+				String str = br.readLine();
+				if(rslt)
+					rslt = trie.insert(str);
+			}
+			if(!rslt) 
+				sb.append("NO");
+			else 
+				sb.append("YES");
+			sb.append('\n');
+		}
+
+		System.out.println(sb);
+
+	}
+}
