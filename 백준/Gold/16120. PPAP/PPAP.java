0250c9be440eacdacbc0145d4fbd391f57311163
chooorock <105736179+chorockchung@users.noreply.github.com>
2024-06-11 16:46:14 +0900

    [Gold IV] Title: PPAP, Time: 404 ms, Memory: 57296 KB -BaekjoonHub

---
+++ 백준/Gold/16120. PPAP/PPAP.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {	
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(br.readLine());
+		StringBuilder stack = new StringBuilder();
+
+		int stackSize = 0;
+		for(int i = 0; i < sb.length(); i++) {
+			char ch = sb.charAt(i);
+			stack.append(ch);		
+			++stackSize;
+			if(stackSize >= 4 && stack.substring(stackSize - 4, stackSize).equals("PPAP")) {
+				if(i == sb.length() - 1)
+					break;
+				stack.delete(stackSize-3, stackSize); //pap만 삭제
+				stackSize -= 3;
+			}
+		}	
+		
+		if(sb.toString().equals("P"))
+			System.out.println("PPAP");	
+		else if(stackSize != 4)
+			System.out.println("NP");
+		else if(!stack.toString().equals("PPAP"))	
+			System.out.println("NP");
+		else  
+			System.out.println("PPAP");
+	}
+}
