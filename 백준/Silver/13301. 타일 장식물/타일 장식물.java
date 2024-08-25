9d6bb8160b5ad4d074a3e2dbebc38fcb1076caad
chooorock <105736179+chorockchung@users.noreply.github.com>
2024-08-25 17:05:55 +0900

    [Silver V] Title: 타일 장식물, Time: 100 ms, Memory: 14300 KB, Score: 74 point -BaekjoonHub

---
+++ 백준/Silver/13301. 타일 장식물/타일 장식물.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	static int[] dp;
+
+    public static void makeDP(int n) {
+    	dp[0] = dp[1] = 1;
+    	int i = 2;
+    	while(i < n + 2) {
+    		dp[i] = dp[i-2] + dp[i-1];
+    		i++;
+    	}
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        dp = new int[n + 2];
+        makeDP(n);
+        System.out.println(dp[n+1]*2);
+    }
+}
+
